80ba0ae9dc81d4612f34ad02e61372fb81db23d5
tachyonok <tachyonparticle123@gmail.com>
2023-10-12 12:56:46 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -6,51 +6,64 @@ const messages = [
   "Invest your new income🤑",
 ];
 
+// state of each component is independent of states of any other component that is being rendered in the UI
+
 function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   function handlePrevious() {
     if (step > 1) {
-      setStep(step - 1);
+      // setStep(step - 1);
+      // always use a callback while updating state in react
+      setStep((s) => s - 1);
     }
   }
 
   function handleNext() {
     if (step < messages.length) {
-      setStep(step + 1);
+      // setStep(step + 1);
+      setStep((s) => s + 1);
     }
   }
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`${step === 1 ? "active" : ""} ? `}>1</div>
-        <div className={`${step === 2 ? "active" : ""}  `}>2</div>
-        <div className={`${step === 3 ? "active" : ""} `}>3</div>
-      </div>
-      <p className="message">
-        Step : {step} : {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          onClick={handlePrevious}
-          style={{
-            backgroundColor: "#7950f2",
-            color: "#fff",
-          }}
-        >
-          Previous
-        </button>
-        <button
-          onClick={handleNext}
-          style={{
-            backgroundColor: "#7950f2",
-            color: "#fff",
-          }}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+    <>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step === 1 ? "active" : ""} ? `}>1</div>
+            <div className={`${step === 2 ? "active" : ""}  `}>2</div>
+            <div className={`${step === 3 ? "active" : ""} `}>3</div>
+          </div>
+          <p className="message">
+            Step : {step} : {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              onClick={handlePrevious}
+              style={{
+                backgroundColor: "#7950f2",
+                color: "#fff",
+              }}
+            >
+              Previous
+            </button>
+            <button
+              onClick={handleNext}
+              style={{
+                backgroundColor: "#7950f2",
+                color: "#fff",
+              }}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 export default App;
